396324ce477396fde6bc17d3733128548072b092
marco76tv <marco.sottana@gmail.com>
2020-12-30 14:30:17 +0100

    vendor publish force

--- laravel/config/geek-local/xra.php
+++ laravel/config/geek-local/xra.php
@@ -1,28 +1,29 @@
-<?php return array (
-  'adm_theme' => 'sb-admin-2-bs4',
-  'enable_ads' => '1',
-  'model' => 
-  array (
-    'amenity' => 'Modules\Blog\Models\Amenity',
-    'article' => 'Modules\Blog\Models\Article',
-    'bell_boy' => 'Modules\Food\Models\BellBoy',
-    'booking' => 'Modules\Cart\Models\Booking',
-    'cart' => 'Modules\Cart\Models\Cart',
-    'cuisine_cat' => 'Modules\Food\Models\CuisineCat',
-    'doc' => 'Modules\Blog\Models\Doc',
-    'home' => 'Modules\Xot\Models\Home',
-    'location' => 'Modules\Food\Models\Location',
-    'opening_hour' => 'Modules\Food\Models\OpeningHour',
-    'page' => 'Modules\Blog\Models\Page',
-    'photo' => 'Modules\Blog\Models\Photo',
-    'post' => 'Modules\Blog\Models\Post',
-    'profile' => 'Modules\Food\Models\Profile',
-    'rating' => 'Modules\Blog\Models\Rating',
-    'restaurant' => 'Modules\Food\Models\Restaurant',
-    'restaurant_provider' => 'Modules\Food\Models\RestaurantProvider',
-    'tip' => 'Modules\Food\Models\Tip',
-    'widget' => 'Modules\Xot\Models\Widget',
-  ),
-  'primary_lang' => 'it',
-  'pub_theme' => 'bsitalia',
-);
+<?php
+
+ return [
+     'adm_theme' => 'sb-admin-2-bs4',
+     'enable_ads' => '1',
+     'model' => [
+         'amenity' => 'Modules\Blog\Models\Amenity',
+         'article' => 'Modules\Blog\Models\Article',
+         'bell_boy' => 'Modules\Food\Models\BellBoy',
+         'booking' => 'Modules\Cart\Models\Booking',
+         'cart' => 'Modules\Cart\Models\Cart',
+         'cuisine_cat' => 'Modules\Food\Models\CuisineCat',
+         'doc' => 'Modules\Blog\Models\Doc',
+         'home' => 'Modules\Xot\Models\Home',
+         'location' => 'Modules\Food\Models\Location',
+         'opening_hour' => 'Modules\Food\Models\OpeningHour',
+         'page' => 'Modules\Blog\Models\Page',
+         'photo' => 'Modules\Blog\Models\Photo',
+         'post' => 'Modules\Blog\Models\Post',
+         'profile' => 'Modules\Blog\Models\Profile',
+         'rating' => 'Modules\Blog\Models\Rating',
+         'restaurant' => 'Modules\Food\Models\Restaurant',
+         'restaurant_provider' => 'Modules\Food\Models\RestaurantProvider',
+         'tip' => 'Modules\Food\Models\Tip',
+         'widget' => 'Modules\Xot\Models\Widget',
+     ],
+     'primary_lang' => 'it',
+     'pub_theme' => 'bsitaliadocs',
+ ];

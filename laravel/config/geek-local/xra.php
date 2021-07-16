4b785b3b81b6fa94c8da25f6db93f089589d32c2
Malebestia <malebestia@yahoo.it>
2021-07-16 18:48:21 +0200

    .

--- laravel/config/geek-local/xra.php
+++ laravel/config/geek-local/xra.php
@@ -1,26 +1,22 @@
-<?php
-
- return [
-     'adm_theme' => 'sb-admin-2-bs4',
-     'enable_ads' => '1',
-     'model' => [
-         'amenity' => 'Modules\Blog\Models\Amenity',
-         'article' => 'Modules\Blog\Models\Article',
-         'bell_boy' => 'Modules\Food\Models\BellBoy',
-         'booking' => 'Modules\Cart\Models\Booking',
-         'cart' => 'Modules\Cart\Models\Cart',
-         'cuisine_cat' => 'Modules\Food\Models\CuisineCat',
-         'doc' => 'Modules\Blog\Models\Doc',
-         'home' => 'Modules\Xot\Models\Home',
-         'location' => 'Modules\Food\Models\Location',
-         'opening_hour' => 'Modules\Food\Models\OpeningHour',
-         'page' => 'Modules\Blog\Models\Page',
-         'photo' => 'Modules\Blog\Models\Photo',
-         'post' => 'Modules\Blog\Models\Post',
-         'profile' => 'Modules\Blog\Models\Profile',
-     ],
-     'primary_lang' => 'it',
-     //'pub_theme' => 'bsitalia',
-     //'pub_theme' => 'sb-admin-2-bs4',
-     'pub_theme' => 'coderdocs',
- ];
+<?php return array (
+  'adm_theme' => 'sb-admin-2-bs4',
+  'enable_ads' => '1',
+  'model' => 
+  array (
+    'amenity' => 'Modules\Blog\Models\Amenity',
+    'article' => 'Modules\Blog\Models\Article',
+    'bell_boy' => 'Modules\Food\Models\BellBoy',
+    'booking' => 'Modules\Cart\Models\Booking',
+    'cart' => 'Modules\Cart\Models\Cart',
+    'category' => 'Modules\Blog\Models\Category',
+    'doc' => 'Modules\Blog\Models\Doc',
+    'home' => 'Modules\Xot\Models\Home',
+    'page' => 'Modules\Blog\Models\Page',
+    'photo' => 'Modules\Blog\Models\Photo',
+    'post' => 'Modules\Blog\Models\Post',
+    'profile' => 'Modules\Blog\Models\Profile',
+    'tag' => 'Modules\Blog\Models\Tag',
+  ),
+  'primary_lang' => 'it',
+  'pub_theme' => 'coderdocs',
+);

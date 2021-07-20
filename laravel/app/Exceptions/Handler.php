902c6d1f0cf8af71b7a5272b65ef3958da16d410
Malebestia <malebestia@yahoo.it>
2021-07-20 11:10:46 +0200

    .

--- laravel/app/Exceptions/Handler.php
+++ laravel/app/Exceptions/Handler.php
@@ -3,16 +3,15 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Log;
 
-class Handler extends ExceptionHandler
-{
+class Handler extends ExceptionHandler {
     /**
      * A list of the exception types that are not reported.
      *
      * @var array
      */
     protected $dontReport = [
-        //
     ];
 
     /**
@@ -25,13 +24,27 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
+    public function render($request, Throwable $exception) {
+        Log::info($exception->getMessage(), [
+            'url' => Request::url(),
+            'input' => Request::all(),
+        ]);
+
+        if ($this->isHttpException($exception)) {
+            $err_view = 'pub_theme::errors.'.$exception->getStatusCode();
+            if (view()->exists($err_view)) {
+                return response()->view($err_view, ['message' => $exception->getMessage()], $exception->getStatusCode());
+            }
+        }
+
+        return parent::render($request, $exception);
+    }
+
     /**
      * Register the exception handling callbacks for the application.
      *
      * @return void
      */
-    public function register()
-    {
-        //
+    public function register() {
     }
 }
